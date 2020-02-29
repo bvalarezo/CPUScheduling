c5788b2cccc7040d9dbd73fe7a4086904f64119b
Bryan <bryan.valarezo24@gmail.com>
2020-02-29 13:29:25 -0500

    Initialized all the files for this project.

---
+++ Threads/TimerInterruptHandler.java
@@ -0,0 +1,42 @@
+package osp.Threads;
+
+import osp.IFLModules.*;
+import osp.Utilities.*;
+import osp.Hardware.*;
+
+/**
+ * Name: Bryan Valarezo
+ * StudentID: 110362410
+ * 
+ * I pledge my honor that all parts of this project were done by me individually, without 
+ * collaboration with anyone, and without consulting any external sources that provide 
+ * full or partial solutions to a similar project. 
+ * I understand that breaking this pledge will result in an “F” for the entire course.
+ */
+
+/**
+ * The timer interrupt handler. This class is called upon to handle timer
+ * interrupts.
+ * 
+ * @OSPProject Threads
+ */
+public class TimerInterruptHandler extends IflTimerInterruptHandler {
+    /**
+     * This basically only needs to reset the times and dispatch another process.
+     * 
+     * @OSPProject Threads
+     */
+    public void do_handleInterrupt() {
+        // your code goes here
+
+    }
+
+    /*
+     * Feel free to add methods/fields to improve the readability of your code
+     */
+
+}
+
+/*
+ * Feel free to add local classes to improve the readability of your code
+ */
